0397ebea084a08f77c5de770906f204788ef6119
lemon <1020392494@qq.com>
2019-10-31 16:49:05 +0800

    新增了一句话

---
+++ gmall-user-manage/src/main/java/com/atguigu/gmall/gmallusermanage/GmallUserManageApplication.java
@@ -0,0 +1,15 @@
+package com.atguigu.gmall.gmallusermanage;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan(basePackages = "com.atguigu.gmall.gmallusermanage.mapper")
+public class GmallUserManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallUserManageApplication.class, args);
+    }
+
+}
